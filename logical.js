7116e55874d38d0098164f6a01f80510db491630
Janani0525 <sriijanani9@gmail.com>
2026-06-12 13:59:20 +0530

    logical.js

---
+++ logical.js
@@ -0,0 +1,9 @@
+console.log("logical operators")
+a=30
+b=40
+console.log(a > 0 && b > 0)
+console.log(a > 0 && b < 0)
+console.log(a < 0 || b < 0)
+console.log(a < 0 || b > 0)
+console.log(!(a < 0))
+console.log(!(b < 0))
