b78888a11f784591a01362a11810b5442bb0227a
Janani0525 <sriijanani9@gmail.com>
2026-06-12 15:48:20 +0530

    functions.js

---
+++ functions.js
@@ -0,0 +1,13 @@
+//Non-parametric functions
+function greet(){
+    console.log("Hello,World!");
+}
+greet();//Output : Hello,World!
+
+//Parametric functions
+function add(a,b){
+    
+    return a+b;
+
+}
+console.log(add(5,3));
